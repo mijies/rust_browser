055e288608b3020de5ba690c182247a874d983f6
seijimatsuda <seiji3030@gmail.com>
2019-09-04 10:35:00 +0900

    [dawdle] walk through html with html5ever

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+use clap::{App, Arg};
+
+extern crate rust_browser;
+use rust_browser::renderer;
+
+const VERSION_STR: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("rust_browser")
+        .version(VERSION_STR)
+        .author("mijies")
+        .about("Web browser implementation in Rust")
+        .arg(Arg::with_name("FILE")
+            .help("Input file")
+            .index(1)
+        );
+    let _app_matches = app.get_matches();
+    renderer::f();
 }
---
+++ src/renderer.rs
@@ -0,0 +1,65 @@
+use std::default::Default;
+use std::io::BufReader;
+use std::iter;
+use html5ever::parse_document;
+use html5ever::rcdom::{Handle, NodeData, RcDom};
+use html5ever::tendril::TendrilSink;
+
+fn walk(indent: usize, node: Handle) {
+
+    fn escape_default(s: &str) -> String {
+        s.chars().flat_map(|c| c.escape_default()).collect()
+    }
+
+    print!("{}", iter::repeat(" ").take(indent).collect::<String>());
+
+    match node.data {
+        NodeData::Document => println!("#Dcoument"),
+        NodeData::Doctype {
+            ref name,
+            ref public_id,
+            ref system_id,
+        } => println!("<!DOCTYPE {} \"{}\" \"{}\">", name, public_id, system_id),
+        NodeData::Text { ref contents } => {
+            println!("#text: {}", escape_default(&contents.borrow()))
+        }
+        NodeData::Comment { ref contents } => println!("<!-- {} -->", escape_default(contents)),
+        NodeData::Element {
+            ref name,
+            ref attrs,
+            ..
+        } => {
+            assert!(name.ns == ns!(html));
+            print!("<{}", name.local);
+            for attr in attrs.borrow().iter() {
+                assert!(attr.name.ns == ns!());
+                print!(" {}=\"{}\"", attr.name.local, attr.value);
+            }
+            println!(">");
+        }
+        NodeData::ProcessingInstruction { .. } => unreachable!(),
+    }
+
+    for child in node.children.borrow().iter() {
+        walk(indent + 2, child.clone());
+    }
+}
+
+pub fn f() {
+    let text = "
+<html>
+<body>
+hello
+<font color=\"red\">web</font>
+world!
+</body>
+</html>
+"
+    .to_owned();
+
+    let dom = parse_document(RcDom::default(), Default::default())
+        .from_utf8()
+        .read_from(&mut BufReader::new(text.as_bytes()))
+        .unwrap();
+    walk(0, dom.document);
+}
