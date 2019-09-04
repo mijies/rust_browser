67133f970b1dd797ba4a9d5b45ac85e78550ca93
seijimatsuda <seiji3030@gmail.com>
2019-09-04 11:44:18 +0900

    [change] walk through html from file

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
-// extern crate clap;
-// use clap::{App, Arg};
 
 extern crate rust_browser;
 use rust_browser::renderer;
 
-// const VERSION_STR: &'static str = env!("CARGO_PKG_VERSION");
+extern crate clap;
+use clap::{App, Arg};
+
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
+const VERSION_STR: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    // let app = App::new("rust_browser")
-    //     .version(VERSION_STR)
-    //     .author("mijies")
-    //     .about("Web browser implementation in Rust")
-    //     .arg(Arg::with_name("FILE")
-    //         .help("Input file")
-    //         .index(1)
-    //     );
-    // let _app_matches = app.get_matches();
-    renderer::f();
+    let app = App::new("rust_browser")
+        .version(VERSION_STR)
+        .author("mijies")
+        .about("Web browser implementation in Rust")
+        .arg(Arg::with_name("FILE")
+            .help("Input file")
+            .index(1)
+        );
+    let _app_matches = app.get_matches();
+    
+    let mut html_source_file = OpenOptions::new()
+        .read(true)
+        .open("./example/test.html")
+        .unwrap();
+    let mut html_source = "".to_string();
+    html_source_file
+        .read_to_string(&mut html_source)
+        .ok()
+        .expect("cannot read file");
+    renderer::show_html(html_source.as_str());
 }
