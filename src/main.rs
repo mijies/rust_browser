fe1af012ea64eea752260b8fd5c2773bed837139
seijimatsuda <seiji3030@gmail.com>
2019-09-04 11:21:03 +0900

    [add] src/css.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-extern crate clap;
-use clap::{App, Arg};
+// extern crate clap;
+// use clap::{App, Arg};
 
 extern crate rust_browser;
 use rust_browser::renderer;
 
-const VERSION_STR: &'static str = env!("CARGO_PKG_VERSION");
+// const VERSION_STR: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    let app = App::new("rust_browser")
-        .version(VERSION_STR)
-        .author("mijies")
-        .about("Web browser implementation in Rust")
-        .arg(Arg::with_name("FILE")
-            .help("Input file")
-            .index(1)
-        );
-    let _app_matches = app.get_matches();
+    // let app = App::new("rust_browser")
+    //     .version(VERSION_STR)
+    //     .author("mijies")
+    //     .about("Web browser implementation in Rust")
+    //     .arg(Arg::with_name("FILE")
+    //         .help("Input file")
+    //         .index(1)
+    //     );
+    // let _app_matches = app.get_matches();
     renderer::f();
 }
